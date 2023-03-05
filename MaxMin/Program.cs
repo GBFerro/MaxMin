2b6801e7960118302af833e73b68c2c0329ea665
GBFerro <giovani.ferro@usp.br>
2023-03-05 18:34:39 -0300

    Add project files.

---
+++ MaxMin/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i , j;
+        float[] numeros = new float[15];
+        float max, min;
+
+        for (i = 0; i < 15; i++)
+        {
+            Console.Write($"Digite o {i+1}º número: ");
+            numeros[i] = float.Parse(Console.ReadLine());
+        }
+
+        i = 1;
+        max = numeros[0];
+        while (i < 15)
+        {
+            if (numeros[i] > max)
+            {
+                max = numeros[i];
+            }
+            i++;
+        }
+
+        min = numeros[0];
+        j = 1;
+        do
+        {
+            if (numeros[j] < min)
+            {
+                min = numeros[j];
+            }
+            j++;
+        } while (j < 15);
+
+        Console.WriteLine("Maior e menor numero: " + max + " e " + min);
+    }
+}
